d310311a6205c60ac0d3ef6c759f6967113c5cb7
NRiddick113 <NatashaRiddick@pursuit.org>
2025-04-16 17:19:28 -0400

    Add Rainbow feature

--- color-splash.js
+++ color-splash.js
@@ -1,6 +1,8 @@
 const grid = document.getElementById('grid');
 const colorPicker = document.getElementById('colorPicker');
 let brushSize = 1;
+let isRainbowMode = false;
+
 
 
 // Create the grid
@@ -33,7 +35,9 @@ function paintCellsAround(index, size) {
       for (let c = col - half; c <= col + half; c++) {
         const neighborIndex = r * gridSize + c;
         if (r >= 0 && r < gridSize && c >= 0 && c < gridSize && cells[neighborIndex]) {
-          cells[neighborIndex].style.backgroundColor = colorPicker.value;
+            const color = isRainbowMode ? getRandomColor() : colorPicker.value;
+            cells[neighborIndex].style.backgroundColor = color;
+            
             cells[neighborIndex].classList.add('flash');
              setTimeout(() => {
              cells[neighborIndex].classList.remove('flash');
@@ -42,11 +46,28 @@ function paintCellsAround(index, size) {
       }
     }
   }
-  
+  // Add Brush Sizes
   function setBrushSize(size) {
     brushSize = size;
     const label = size === 1 ? "Small" : size === 3 ? "Medium" : "Large";
     document.getElementById('brushDisplay').innerText = `Current Brush Size: ${label}`;
   }
+
+// Add rainbow feature
+  function toggleRainbow() {
+    isRainbowMode = !isRainbowMode;
+    document.getElementById('rainbowStatus').innerText = `Rainbow Mode: ${isRainbowMode ? "ON" : "OFF"}`;
+  }
+
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  
   
 
